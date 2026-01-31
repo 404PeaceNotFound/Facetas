87595b3b4930d3eca13542eac9915240d6b0df72
Vitor Costa <210616944+404PeaceNotFound@users.noreply.github.com>
2026-01-31 11:10:46 -0300

    1

---
+++ code/main.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+
+screen_width = 1280
+screen_height = 720
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Facetas')
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
+
