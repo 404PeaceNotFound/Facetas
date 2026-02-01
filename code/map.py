cba388f17fe77bbe72edb56dd67c02a19412fbc0
ygcs796 <ygcs@cin.ufpe.br>
2026-02-01 11:17:11 -0300

    coloquei a imagem de fundo na tela principal do jogo

--- code/map.py
+++ code/map.py
@@ -25,9 +25,22 @@ class GameMap:
         self.height = MAP_HEIGHT
         self.floor_y = 500
 
+        try:
+            self.bg_image = pygame.image.load('code/assets\images\cenario2.png').convert_alpha()
+            # Opcional: Redimensionar para a altura da tela mantendo a proporção ou forçando
+            # Aqui forçamos a largura do mapa (2000) e altura da tela (600)
+            self.bg_image = pygame.transform.scale(self.bg_image, (self.width, self.height))
+        except:
+            print("Erro ao carregar assets/cenario2.png. Usando fundo sólido.")
+            self.bg_image = None
+
     def draw(self, screen, cam_x):
-        # fundo e chão
-        screen.fill((50, 50, 70))
+        if self.bg_image:
+            # Desenhamos o fundo aplicando o deslocamento da câmera
+            screen.blit(self.bg_image, (cam_x, 0))
+        else:
+            # Fundo padrão caso a imagem falhe
+            screen.fill((50, 50, 70))
         pygame.draw.rect(screen, (30, 30, 30),
                          (cam_x, self.floor_y, self.width, self.height - self.floor_y))
         # bordas visuais
